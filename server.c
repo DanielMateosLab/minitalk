8ba08a01c614a266bdab3b53a03b9824fbf434a6
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-06-26 23:09:58 +0200

    Finish server logic

--- server.c
+++ server.c
@@ -6,33 +6,42 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 08:08:39 by damateos          #+#    #+#             */
-/*   Updated: 2024/06/24 23:28:10 by damateos         ###   ########.fr       */
+/*   Updated: 2024/06/26 23:09:06 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 // TODO: switch hm_ prefix for ft_
+// TODO: test ft_expand_str safe free calls
 
-typedef struct s_mt_state
+typedef struct s_message
 {
 	pid_t	sender;
 	int		bit;
 	int		pending;
-}	t_mt_state;
+}	t_message;
 
-volatile struct s_mt_state state;
+typedef struct s_buffer
+{
+	char	*ptr;
+	int		len;
+	int		si;
+	int		bi;
+} 	t_buffer;
+
+volatile struct s_message	g_message;
 
 void	action(int sig, siginfo_t info, void *context)
 {
-	if (state.pending)
+	if (g_message.pending)
 		return ;
-	state.pending = 1;
-	state.sender = info.si_pid;
+	g_message.pending = 1;
+	g_message.sender = info.si_pid;
 	if (sig == SIGUSR1)
-		state.bit = 0;
+		g_message.bit = 0;
 	else
-		state.bit = 1;
+		g_message.bit = 1;
 	return ;
 }
 
@@ -52,7 +61,7 @@ char	*ft_expand_str(char **str, size_t len, size_t new_len)
 	temp = ft_calloc(new_len, sizeof(char));
 	if (!temp)
 	{
-		ft_safe_free(*str);
+		ft_safe_free(str);
 		return (NULL);
 	}
 	i = 0;
@@ -61,51 +70,70 @@ char	*ft_expand_str(char **str, size_t len, size_t new_len)
 		temp[i] = *str[i];
 		i++;
 	}
-	ft_safe_free(*str);
+	ft_safe_free(str);
 	*str = temp;
 }
 
-int	main()
+void	init_str_state(struct s_buffer *buff)
+{
+	buff->len = 1024;
+	buff->bi = 0;
+	buff->si = 0;
+	buff->ptr = (char *)ft_calloc(buff->len, sizeof(char));
+}
+
+void	print_str_and_reset_state(struct s_buffer *buff)
+{
+	write(1, buff->ptr, ft_strlen(buff->ptr));
+	ft_safe_free(&buff->ptr);
+	init_str_state(buff);
+}
+
+int	main(void)
 {
 	struct sigaction	sa;
-	char				*str;
-	int					str_i;
-	int					bit_i;
-	int					str_len;
+	struct s_buffer		*buff;
 
-	str_len = 1024;
-	str = (char *)ft_calloc(str_len, sizeof(char));
-	if (!str)
+	init_str_state(buff);
+	if (!buff->ptr)
 		return (1);
-	str_i = 0;
-	bit_i = 0;
 	ft_memset(&sa, 0, sizeof(sa));
-	ft_memset(&state, 0, siezof(state));
+	ft_memset(&g_message, 0, siezof(g_message));
 	sa.sa_sigaction = action;
 	sa.sa_flags = SA_SIGINFO;
 	while (1)
 	{
-		if (state.pending)
+		if (g_message.pending)
 		{
-			if (state.bit)
-				str[str_i] |= (1 << bit_i);
+			if (g_message.bit)
+				buff->ptr[buff->si] |= (1 << buff->bi);
 			else
-				str[str_i] &= ~(1 << bit_i);
-			if (++bit_i == 7)
+				buff->ptr[buff->si] &= ~(1 << buff->bi);
+			if (buff->bi == 7)
 			{
-				// if char is \0, print and clear string.
-				str_i++;
-				bit_i = 0;
+				if (!buff->ptr[buff->si])
+				{
+					print_str_and_reset_state(&buff);
+					if (!buff->ptr)
+						return (1);
+				}
+				else
+				{
+					buff->si++;
+					buff->bi = 0;
+				}
 			}
-			if (str_i == str_len - 1)
+			else
+				buff->bi++;
+			if (buff->si == buff->len - 1)
 			{
-				ft_expand_str(&str, str_len, (str_len *= 2));
-				if (!str)
+				ft_expand_str(&buff->ptr, buff->len, (buff->len *= 2));
+				if (!buff->ptr)
 					return (1);
 			}
-			// send confirmation
-			// wait for signal
+			kill(g_message.sender, SIGUSR1);
 		}
+		pause();
 	}
 	return (0);
 }
