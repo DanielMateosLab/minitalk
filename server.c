bc3ac51c5bcc996f6b36044540389084af246597
Daniel Mateos Labrador <daniel.mateos@asafedigital.com>
2024-07-01 23:27:56 +0200

    General refactor of server.c

--- server.c
+++ server.c
@@ -6,14 +6,11 @@
 /*   By: damateos <damateos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 08:08:39 by damateos          #+#    #+#             */
-/*   Updated: 2024/06/26 23:09:06 by damateos         ###   ########.fr       */
+/*   Updated: 2024/07/01 23:27:34 by damateos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-
-// TODO: switch hm_ prefix for ft_
-// TODO: test ft_expand_str safe free calls
 
 typedef struct s_message
 {
@@ -30,27 +27,20 @@ typedef struct s_buffer
 	int		bi;
 } 	t_buffer;
 
-volatile struct s_message	g_message;
+volatile t_message	*g_message;
 
-void	action(int sig, siginfo_t info, void *context)
+void	action(int sig, siginfo_t *info, void *context)
 {
-	if (g_message.pending)
+	(void)context;
+	if (g_message->pending)
 		return ;
-	g_message.pending = 1;
-	g_message.sender = info.si_pid;
+	g_message->pending = 1;
+	g_message->sender = info->si_pid;
 	if (sig == SIGUSR1)
-		g_message.bit = 0;
+		g_message->bit = 0;
 	else
-		g_message.bit = 1;
+		g_message->bit = 1;
 	return ;
-}
-
-void	ft_safe_free(void **ptr)
-{
-	if (!*ptr)
-		return ;
-	free(*ptr);
-	*ptr = NULL;
 }
 
 char	*ft_expand_str(char **str, size_t len, size_t new_len)
@@ -61,7 +51,7 @@ char	*ft_expand_str(char **str, size_t len, size_t new_len)
 	temp = ft_calloc(new_len, sizeof(char));
 	if (!temp)
 	{
-		ft_safe_free(str);
+		ft_free((void **)str);
 		return (NULL);
 	}
 	i = 0;
@@ -70,8 +60,9 @@ char	*ft_expand_str(char **str, size_t len, size_t new_len)
 		temp[i] = *str[i];
 		i++;
 	}
-	ft_safe_free(str);
+	ft_free((void **)str);
 	*str = temp;
+	return (*str);
 }
 
 void	init_str_state(struct s_buffer *buff)
@@ -82,56 +73,67 @@ void	init_str_state(struct s_buffer *buff)
 	buff->ptr = (char *)ft_calloc(buff->len, sizeof(char));
 }
 
-void	print_str_and_reset_state(struct s_buffer *buff)
+void	print_str_and_reset_state(t_buffer *buff)
 {
 	write(1, buff->ptr, ft_strlen(buff->ptr));
-	ft_safe_free(&buff->ptr);
+	ft_free((void **)&buff->ptr);
 	init_str_state(buff);
+}
+
+int	process_message(t_buffer *buff)
+{
+	if (g_message->bit)
+		buff->ptr[buff->si] |= (1 << buff->bi);
+	else
+		buff->ptr[buff->si] &= ~(1 << buff->bi);
+	if (buff->bi == 7)
+	{
+		if (!buff->ptr[buff->si])
+		{
+			print_str_and_reset_state(buff);
+			if (!buff->ptr)
+				return (1);
+		}
+		else
+		{
+			buff->si++;
+			buff->bi = 0;
+		}
+	}
+	else
+		buff->bi++;
+	if (buff->si == buff->len - 1)
+	{
+		ft_expand_str(&buff->ptr, buff->len, (buff->len *= 2));
+		if (!buff->ptr)
+			return (1);
+	}
+	kill(g_message->sender, SIGUSR1);
+	return (0);
 }
 
 int	main(void)
 {
 	struct sigaction	sa;
-	struct s_buffer		*buff;
+	t_buffer			*buff;
+	char				*pid;
 
 	init_str_state(buff);
 	if (!buff->ptr)
 		return (1);
 	ft_memset(&sa, 0, sizeof(sa));
-	ft_memset(&g_message, 0, siezof(g_message));
+	ft_memset(&g_message, 0, sizeof(g_message));
 	sa.sa_sigaction = action;
 	sa.sa_flags = SA_SIGINFO;
+	pid = ft_itoa((int)getpid());
+	write(1, pid, ft_strlen(pid));
+	ft_putchar_fd('\n', 1);
 	while (1)
 	{
-		if (g_message.pending)
+		if (g_message->pending)
 		{
-			if (g_message.bit)
-				buff->ptr[buff->si] |= (1 << buff->bi);
-			else
-				buff->ptr[buff->si] &= ~(1 << buff->bi);
-			if (buff->bi == 7)
-			{
-				if (!buff->ptr[buff->si])
-				{
-					print_str_and_reset_state(&buff);
-					if (!buff->ptr)
-						return (1);
-				}
-				else
-				{
-					buff->si++;
-					buff->bi = 0;
-				}
-			}
-			else
-				buff->bi++;
-			if (buff->si == buff->len - 1)
-			{
-				ft_expand_str(&buff->ptr, buff->len, (buff->len *= 2));
-				if (!buff->ptr)
-					return (1);
-			}
-			kill(g_message.sender, SIGUSR1);
+			if (process_message(buff) == 1)
+				return (1);
 		}
 		pause();
 	}
